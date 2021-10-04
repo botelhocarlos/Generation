6584f8f5ed7bf2ca3e41528766801ce7850a9f4c
botelhocarlos <emaildocarlosbotelho@gmail.com>
2021-10-04 11:45:49 -0300

    alterando exercício 2

--- Exercícios JAVA 04-10/EXERC02.java
+++ Exercícios JAVA 04-10/EXERC02.java
@@ -15,25 +15,25 @@ public class EXERC02 {
 		System.out.println("\nEntre com o terceiro número: ");
 		num3 = leia.nextInt();
 		
-		if (num1>=num2 && num2>=num3){
+		if (num1<=num2 && num2<=num3){
 			System.out.println("A ordem crescente é: "+num1+", "+num2+", "+num3);
 		}
-		else if (num1>=num3 && num3>=num2) {
+		else if (num1<=num3 && num3<=num2) {
 			System.out.println("A ordem crescente é: "+num1+", "+num3+", "+num2);
 		}
-		else if (num2>=num1 && num1>=num3) {
+		else if (num2<=num1 && num1<=num3) {
 			System.out.println("A ordem crescente é: "+num2+", "+num1+", "+num3);
 		}
-		else if (num2>=num3 && num3>=num1) {
+		else if (num2<=num3 && num3<=num1) {
 			System.out.println("A ordem crescente é: "+num2+", "+num3+", "+num1);
 		}
-		else if (num3>=num1 && num1>=num2) {
+		else if (num3<=num1 && num1<=num2) {
 			System.out.println("A ordem crescente é: "+num3+", "+num1+", "+num2);
 		}
-		else if (num3>=num2 && num2>=num1) {
+		else if (num3<=num2 && num2<=num1) {
 			System.out.println("A ordem crescente é: "+num3+", "+num2+", "+num1);
 		}
-		else if (num3>=num1 && num1>=num1) {
+		else if (num3<=num1 && num1<=num1) {
 			System.out.println("A ordem crescente é: "+num3+", "+num1+", "+num2);
 		}
 		
