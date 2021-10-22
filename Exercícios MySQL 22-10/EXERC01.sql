e4ea2e953e84a3bb912b9630237e6ec65e81ed52
botelhocarlos <emaildocarlosbotelho@gmail.com>
2021-10-22 13:27:01 -0300

    corringindo

--- Exercícios MySQL 22-10/EXERC01.sql
+++ Exercícios MySQL 22-10/EXERC01.sql
@@ -1,1 +1,19 @@
-update tb_funcionarios set salario = 2650 where id =5;
+create database	db_rh;
+use db_rh;
+create table tb_funcionaries(
+	id int(5) auto_increment,
+    nome varchar(30) not null,
+    genero enum('ela', 'elu', 'ele', 'outro'),
+    dataNascimento date,
+    salario int(6),
+    primary key (id));
+insert into tb_funcionaries (nome, genero, datanascimento, salario) values ("Joana", "ela", '1995-05-10', 2500);
+insert into tb_funcionaries (nome, genero, datanascimento, salario) values ("Raí", "elu", '1996-02-04', 9100);
+insert into tb_funcionaries (nome, genero, datanascimento, salario) values ("Luan", "ele", '1999-10-12', 1500);
+insert into tb_funcionaries (nome, genero, datanascimento, salario) values ("Carol", "ela", '1984-11-14', 6000);
+insert into tb_funcionaries (nome, genero, datanascimento, salario) values ("Til", "outro", '1994-09-28', 8500);
+select * from tb_funcionaries;
+    
+select * from tb_funcionaries where salario > 2000;
+select * from tb_funcionaries where salario < 2000;
+update funcionaries set salario = 9500 where id = 5;
--- Exercícios MySQL 22-10/EXERC03.sql
+++ Exercícios MySQL 22-10/EXERC03.sql
@@ -1,2 +1,20 @@
-update tb_alunes set nota = 8 where id = 4;
+create	 	database	db_escola;
+use db_escola;
+create table tb_alunes(
+	id int(5) auto_increment,
+    nome varchar(30) not null,
+    genero enum('ela', 'elu', 'ele', 'outro'),
+    dataNascimento date,
+    nota int(6),
+    primary key (id));
+insert into tb_alunes (nome, genero, datanascimento, nota) values ("Joana", "ela", '1995-05-10', 6);
+insert into tb_alunes (nome, genero, datanascimento, nota) values ("Raí", "elu", '1996-02-04', 7);
+insert into tb_alunes (nome, genero, datanascimento, nota) values ("Luan", "ele", '1999-10-12', 8);
+insert into tb_alunes (nome, genero, datanascimento, nota) values ("Carol", "ela", '1984-11-14', 5);
+insert into tb_alunes (nome, genero, datanascimento, nota) values ("Til", "outro", '1994-09-28', 10);
+select * from tb_alunes;
+    
+select * from tb_alunes where nota > 7;
+select * from tb_alunes where nota < 7;
+update tb_alunes set nota = 8 where id = 5;
 
